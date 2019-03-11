0e31546585687148313083d132fa85f072102f5f
12345678910111213141519 <hasinikeshala@gmail.com>
2019-03-11 09:20:40 +0530

    hjklll

--- app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
+++ app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
@@ -10,28 +10,44 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private int counter=99;
+    private static final String CURRENT_COUNTER ="counter";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState !=null) {
+            counter = savedInstanceState.getInt(CURRENT_COUNTER);
+        }
         countDown();
     }
 
+
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(CURRENT_COUNTER,counter);
+    }
+
     private void countDown(){
-        final TextView textView=findViewById(R.id.textView);
+
 
         final Handler handler=new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
-                    textView.setText(Integer.toString(counter));
-                    counter--;
-                    handler.postDelayed(this,1000);
+                final TextView textView = findViewById(R.id.textView);
+                textView.setText(Integer.toString(counter));
+                if (counter == 0) {
+                    counter = 99;
+                }
 
-                    if(counter==0){
-                        counter=99;
-                    }
+                counter--;
+                handler.postDelayed(this, 1000);
+
+
             }
         });
     }
